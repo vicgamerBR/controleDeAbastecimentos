7d106e8801596fae5c6ede7af02f56b3d54867a1
viquitu <vhreis13@gmail.com>
2021-02-17 21:13:27 -0300

    criação da segunda pagina e teste do script

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function login(){
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    alert(email, senha);
+    window.location.href = "main-page.html"
+}
